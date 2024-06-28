68a1e38f436136aa5180727570ddb5377584807f
Anthony HAREL <a.harel@polysoude.com>
2024-06-28 16:12:19 +0200

    Prmière version de l'appli issure de https://www.youtube.com/watch?v=mMnN4aKaPKI&list=PLFZAa7EupbB5r2oPtDzN0DY2D0jym9Ih_&index=7

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,55 @@
+<template>
+  <q-page class="row">
+    <q-card class="col-xs-9">
+      <q-toolbar>
+        <q-input
+          dense
+          placeholder="Search"
+          borderless
+          class="full-width"
+          :model-value="search"
+        >
+          <template #append>
+            <q-icon name="mdi-magnify"></q-icon>
+          </template>
+        </q-input>
+      </q-toolbar>
+      <q-list bordered>
+        <q-item>
+          <q-item-section side>
+            <q-checkbox :model-value="true"></q-checkbox>
+          </q-item-section>
+          <q-item-section> Go shopping </q-item-section>
+          <q-item-section side>
+            <q-btn flat round size="sm" icon="mdi-delete"></q-btn>
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </q-card>
+    <div class="col-xs-3">
+      <q-toolbar class="bg-primary">
+        <q-btn
+          fab
+          style="margin-bottom: -42px"
+          class="q-ml-lg"
+          color="secondary"
+          icon="mdi-plus"
+        ></q-btn>
+      </q-toolbar>
+    </div>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "IndexPage",
+
+  data() {
+    return {
+      search: "",
+    };
+  },
+});
+</script>
---
+++ src/pages/LoginPage.vue
@@ -0,0 +1,29 @@
+<template>
+  <q-page class="flex flex-center" padding>
+    <div class="full-width" style="max-width: 300px">
+      <h5 class="q-my-md text-center">Login</h5>
+      <q-card>
+        <q-card-section>
+          <q-form>
+            <q-input
+              filled
+              label="email"
+              placeholder="email@example.com"
+              class="q-mb-md"
+            />
+            <q-input filled label="password" type="password" class="q-mb-sm" />
+          </q-form>
+        </q-card-section>
+        <q-checkbox label="remember me" :model-value="true"></q-checkbox>
+
+        <q-btn label="login" class="full-width" color="primary"></q-btn>
+      </q-card>
+    </div>
+  </q-page>
+</template>
+
+<script>
+export default {
+  // name: 'PageName',
+};
+</script>
